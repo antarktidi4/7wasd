c982dee0fc4f176e2f3025f2351a7ebf1fd2eabe
hash <maksim23090421@gmail.com>
2023-05-07 21:00:39 +0700

    5.7.23
    - add error throw in content chrome.runtime.sendMessage.sendMessage() => _.error
    - add timeout in waitForElement (10s)
    - move chat builder into Chat class
    - add debug info in popup (press shift while it is open)
    - add emote offset (RainTime, SteerR, etc.)

--- src/content/components/emote.js
+++ src/content/components/emote.js
@@ -3,5 +3,5 @@ import { html } from "htm/preact";
 export function Emote({emote, onClick = null}) {
   const zerowidth = !onClick && emote?.visibility_simple?.indexOf("ZERO_WIDTH") > -1 ? `margin-left:-${emote.height[0]}px;` : null;
 
-  return html`<img onclick=${onClick} src=${emote.urls[0][1]} alt=${emote.name} class="sevenwasd-emote" style="${zerowidth}"></img>`
+  return html`<img onclick=${onClick} src=${emote.urls[0][1]} alt=${emote.name} title="${emote.name}" class="sevenwasd-emote" style="${zerowidth}"></img>`
 }
